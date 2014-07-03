13ea0f3524a7e565d389bbe246ad24a225302f87
Ubuntu <nskelsey@gmail.com>
2014-07-03 02:50:35 +0000

    Tiny ui tweaks to netpane, it is still ugly

--- web/static/netpane.js
+++ web/static/netpane.js
@@ -1,7 +1,7 @@
 function NetPane(selection) {
-    var margin = {top: 30, right: 20, bottom: 20, left: 40},
-    height = 240 - margin.right - margin.left,
-    width = 800 - margin.top - margin.bottom,
+    var margin = {top: 30, right: 20, bottom: 20, left: 50},
+    height = 240 - margin.top - margin.bottom,
+    width = 800 - margin.right - margin.left,
     color = undefined,
     timeBack = -1; // defines a one minute window
 
@@ -14,15 +14,47 @@ function NetPane(selection) {
                 .attr("width", width + margin.right + margin.left)
                 .attr("height", height + margin.top + margin.bottom);
 
-            this.g = svg.append("g")
+            chart.g = svg.append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
                 .attr("width", width) 
                 .attr("height", height);
 
+            //
+            var yDom = [34, 170, 350, 10000];
+            var yRan = [height, 2*height/3, height/3, 0];
+
             // Build scales
-            this.yScale = d3.scale.linear()
-                .domain([0, 1000])
-                .range([height, 0])
+            chart.yScale = d3.scale.linear()
+                .domain(yDom)
+                .range(yRan)
+                .clamp(true);
+
+            y = chart.yScale;
+
+            sizeBox = svg.append("g")
+                .attr("transform", "translate("+(margin.left)+","+margin.top+")")
+                .attr("class", "scale-box")
+
+            sizeBox.append("rect")
+                .attr("class", "small")
+                .attr("x", 0)
+                .attr("y", y(yDom[1]) + 5)
+                .attr("width", margin.left/2)
+                .attr("height", y(yDom[1]) - y(yDom[2]) - 5);
+
+            sizeBox.append("rect")
+                .attr("class", "avg")
+                .attr("x", 0)
+                .attr("y", y(yDom[2]))
+                .attr("width", margin.left/2)
+                .attr("height", y(yDom[1]) - y(yDom[2]));
+              
+            sizeBox.append("rect")
+                .attr("class", "large")
+                .attr("x", 0)
+                .attr("y", y(yDom[3]))
+                .attr("width", margin.left/2)
+                .attr("height", y(yDom[2]) - y(yDom[3]) - 5);
 
             var now = new Date();
             var past = d3.time.minute.offset(now, -1);
@@ -30,7 +62,7 @@ function NetPane(selection) {
                 .domain(timeWindow())
                 .range([0, width]);
 
-            this.tScale = tScale;
+            chart.tScale = tScale;
 
             var xAxis = d3.svg.axis()
                 .scale(chart.tScale)
@@ -40,11 +72,12 @@ function NetPane(selection) {
             var yAxis = d3.svg.axis()
                 .scale(chart.yScale)
                 .orient("left")
-                .ticks(3);
+                .ticks(2);
 
             svg.append("g")
                 .attr("class", "y axis")
                 .attr("transform", "translate("+margin.left+","+margin.top+")")
+                .call(yAxis);
             
 
             var tOffset = "translate(" + margin.left + "," + (height + margin.top) + ")";
@@ -59,8 +92,8 @@ function NetPane(selection) {
                 chart.tScale.domain(timeWindow());
 
                 tAxisSel.transition()
-                    .duration(500)
                     .ease("linear")
+                    .duration(500)
                     .call(xAxis)
                     .each("end", tick);
             }
@@ -80,7 +113,7 @@ function NetPane(selection) {
     }
 
     chart.update = function(obj){
-        var c = chart
+        var c = chart;
 //        arrdata = Object.keys(c.data).map(function(k){ return c.data[k]; })
 //        
 //        var join = chart.g.selectAll(".elem")
@@ -95,7 +128,7 @@ function NetPane(selection) {
             .on("click", openLink)
             .attr("r", 0)
             .attr("cx", function(d) { 
-                d.start_x = (width - 10) + Math.random()*10;
+                d.start_x = (width - 20) + Math.random()*20;
                 return d.start_x;
             })
             .attr("cy", function(d) { return c.yScale(d.size); })
@@ -104,7 +137,7 @@ function NetPane(selection) {
           .transition()
             .duration(function() { return 250 - 100 + 100*Math.random() })
             .style("opacity", 0.8)
-            .attr("r", 6)
+            .attr("r", 7)
           .transition()
             .ease("linear")
             .duration(60 * 1000)
